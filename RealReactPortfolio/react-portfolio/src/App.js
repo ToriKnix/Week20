8893208078ae3ecb681aff2536ff27587937325f
ToriKnix <toriknixmusic@gmail.com>
2023-08-01 09:56:27 -0600

    I cant figure this out at all. It is saying the importing of Switch is not defined. Taking a break. Have to turn this is today.

--- RealReactPortfolio/react-portfolio/src/App.js
+++ RealReactPortfolio/react-portfolio/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import Header from './components/header';
 import AboutMe from './components/aboutMe';
 import Portfolio from './components/portfolio';
 import Contact from './components/contact';
-import Resume from './resume';
+import Resume from './components/resume';
 import Footer from './components/footer';
 
 function App() {
