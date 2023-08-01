509ca579c74e250212bfb81e77df0f6aaa0ab9e5
ToriKnix <toriknixmusic@gmail.com>
2023-07-31 23:07:57 -0600

    started again

---
+++ RealReactPortfolio/react-portfolio/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation  from './navigation';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Blaine Duran's portfolio
+      </h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
